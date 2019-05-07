929c994bb346372499cbf2f6d17168644dd65212
miladink <mi.aghajohari@gmail.com>
2019-05-07 16:22:45 +0430

    set sepecific task queue

--- cacheback/tasks.py
+++ cacheback/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
+from django.conf import settings
 
 
-@shared_task
+@shared_task(queue=getattr(settings, 'CACHEBACK_CELERY_TASK_QUEUE', 'cacheback'))
 def refresh_cache(klass_str, obj_args, obj_kwargs, call_args, call_kwargs):
     from .base import Job
     Job.perform_async_refresh(klass_str, obj_args, obj_kwargs, call_args, call_kwargs)
